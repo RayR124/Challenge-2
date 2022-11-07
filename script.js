3d6eb3e7369df98547c575694afe9581f2e82309
RayR124 <RealtorRayR@Gmail.com>
2022-11-07 12:42:18 -0800

    Added more HTML, CSS, and the barest of Script
    More complete page now, not perfect, but done for now.

---
+++ script.js
@@ -0,0 +1,7 @@
+const homeTab = document.getElementById("home-tab");
+const expTab = document.getElementById("exp-tab");
+const eduTab = document.getElementById("edu-tab");
+
+const home = document.getElementById("home");
+const exp = document.getElementById("exp");
+const edu = document.getElementById("edu");
